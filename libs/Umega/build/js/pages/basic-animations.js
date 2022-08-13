370ee4ba47cb2650b93b0ba8e3fb9dd10365297c
Sui Gn <natsynapses@gmail.com>
2022-08-12 20:48:24 -0500

    mLearning
    up to date

---
+++ libs/Umega/build/js/pages/basic-animations.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("body").on("click",".animation-demo .btn",function(){$(this).closest(".widget-body").find("img").animo({animation:$(this).text(),duration:$(this).data("duration")||.6})})});
