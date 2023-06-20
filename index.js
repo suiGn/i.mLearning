ad65d219c6629d2d491358e110e6105705e789f4
Sui Gn <natsynapses@gmail.com>
2023-06-20 16:32:03 -0600

    2.0.4
    2.0.4

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-require("/methods");
-console.log("/hello");
+// Load the methods from the src/methods directory
+const method1 = require('./src/methods/methods');
+const waves = require('./src/methods/waves');
+// ... add more methods as needed
+
+// Export the methods
+module.exports = {
+  method1,
+  waves,
+  // ... export more methods as needed
+};
--- src/methods/waves.js
+++ src/methods/waves.js
@@ -1,21 +1,46 @@
-//AddingSinusoids. For more information read AddingSinusoids.md
-
 
 let wave = {
-    _name: 'wave',
-    _frecuency: 0,
-    _range:,
-    _vector: vector
-   
-    get frecuency() {
-      return this._frecuency;
-    },
-   
-    get range(frecuency) {
-      return this._behavior;
-    },
-   
-    incrementBehavior() {
-      this._behavior++;
-    }
+  _name: 'wave',
+  _frequency: 0,
+  _range: '',
+  _vector: '',
+
+  get frequency() {
+    return this._frequency;
+  },
+
+  set frequency(value) {
+    this._frequency = value;
+  },
+
+  get range() {
+    return this._range;
+  },
+
+  set range(value) {
+    this._range = value;
+  },
+
+  incrementFrequency() {
+    this._frequency++;
+  },
+
+  decrementFrequency() {
+    this._frequency--;
   }
+};
+
+module.exports = wave;
+
+/* Now, you can access the wave object 
+and its properties and methods using mlearning.methods.wave. 
+
+For example, you can set the frequency using 
+mlearning.methods.wave.frequency = 10,
+
+ get the range using :
+ mlearning.methods.wave.range, 
+
+ increment the frequency using :
+ mlearning.methods.wave.incrementFrequency(), 
+ and so on. */
