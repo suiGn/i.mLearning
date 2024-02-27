28db11bbc1607a70c3fb64536bbf57c533c01dc7
Sui Gn <natsynapses@gmail.com>
2024-02-27 18:29:57 -0500

    Modular Approach Set
    First stable module version with proper imports/exports structure wise.

--- index.js
+++ index.js
@@ -7,14 +7,14 @@
  * @description Exports the main functionalities of the i.mlearning library.
  */
 
-import LearningModule from './src/LearningModule.js';
+import imLearning from './src/imLearning.js';
 
 /**
  * The main export object of i.mlearning package.
  * @type {Object}
  */
 const imlearning = {
-  LearningModule
+  imLearning
 };
 
 export default imlearning;
---
+++ src/imLearning.js
@@ -0,0 +1,21 @@
+/**
+ * Class representing a learning module.
+ * @description Provides functionalities to manage and organize learning datasets.
+ */
+class imLearning {
+    /**
+     * Initialize a new LearningModule instance.
+     */
+    constructor() {
+        console.log("mLearning initialized");
+    }
+
+    /**
+     * A method to demonstrate functionality.
+     */
+    demonstrate() {
+        console.log("Demonstrating the mLearning functionality.");
+    }
+}
+
+export default imLearning;
