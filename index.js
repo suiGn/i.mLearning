7bc7b587ce14ded333635522f160390a058e376f
Sui Gn <natsynapses@gmail.com>
2024-02-27 17:45:47 -0500

    Clean Start New Structure
    2.3.3 Start using ECMAScript Module (ECMSA) approach and its modular approach for i.mlearning all.this with neurons.me

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Loaded i.mLearning");
+/* In i.mlearning/index.js
+ⓝⓔⓤⓡⓞⓝⓢ.ⓜⓔ
+🆂🆄🅸🅶🅽 */
+
+/**
+ * @module imlearning
+ * @description Exports the main functionalities of the i.mlearning library.
+ */
+
+import LearningModule from './src/LearningModule.js';
+
+/**
+ * The main export object of i.mlearning package.
+ * @type {Object}
+ */
+const imlearning = {
+  LearningModule
+};
+
+export default imlearning;
+console.log("i.mLearning loaded;");
